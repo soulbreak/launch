37d468a538299d335ca162f134f83bae09f09125
soulbreak <daavid.maas@laposte.net>
2020-12-12 15:09:01 +0100

    saving works

--- app/Configuration/Manager.py
+++ app/Configuration/Manager.py
@@ -3,6 +3,7 @@ import re
 from Node import Job, BlkCmd, Cmd, Msg
 from typing import List
 import logging
+import json
 log = logging.getLogger()
 
 class ConfigurationManager(object):
@@ -15,7 +16,7 @@ class ConfigurationManager(object):
         node = self.tree.find("template[@name='{0}']".format(template_name))
         template = ET.tostring(node)
 
-    def load_blkcmd_object(self, node):
+    def load_blkcmd_object(self, node, properties=None):
         if node is None:
             return
         block_attribs = ['retry_end_on_rc', 'maxtime', 'sleep']
@@ -55,42 +56,84 @@ class ConfigurationManager(object):
             created.append(j)
         return created
 
+
+    def load_instance(self, job, template_name, properties) -> Job:
+        created = list()
+        node = self.root.findall("template[@name='{}']".format(template_name))
+        if len(node) != 1:
+            raise RuntimeError("template[@name='{}'] != 1".format(template_name))
+    
+        for item in node.findall('trigger'):
+            logging.debug("Loading {}".format(item.attrib['name']))
+            if item.find('BlkCmd') is not None:
+                blk_to_load = item.find('BlkCmd')
+                setattr(job, item.attrib['name'], self.load_blkcmd_object(blk_to_load))
+            else:
+                log.error('BlkCmd is missing in node {}'.format(item.attrib['name']))
+        created.append(job)
+        return job
+
+    @staticmethod
+    def load_properties(properties):
+        solved = True
+        while solved:
+            solved = False
+            for k, v in properties.items():
+                if v['solved'] is True:
+                    continue
+                text = v['text']
+                matches = re.findall("({{\s*\w+\s*}})", text)
+                if len(matches) == 0:
+                    v['solved'] = True
+                    solved = True
+                    continue
+
+                for match in matches:
+                    variable = re.findall("\w+", match)
+                    if len(variable) != 1:
+                        raise RuntimeError("Bad variable name {}".format(variable))
+                    variable = variable[0]
+                    current_solved = 0
+                    if variable in properties.keys() and properties[variable]['solved'] is True:
+                        text = re.sub(match, properties[variable]['text'] , text)
+                        logging.debug("{} replaced by {}".format(match, text))
+                        solved = True
+                        current_solved += 1
+
+                if len(matches) == current_solved:
+                    v['solved'] = True
+                    solved = True
+                v['text'] = text
+        logging.info("Properties : {}".format(
+            json.dumps(
+                    properties,
+                    sort_keys=True,
+                    indent=4))
+        )
+        if False in [p['solved'] for p in properties.values()]:
+            raise RuntimeError("Some properties are not solved : {}".format(
+                json.dumps(
+                    properties,
+                    sort_keys=True,
+                    indent=4))
+            )
+
+
     def load_node(self, node_name):
         node = self.root.find("node[@name='{0}']".format(node_name))
         if node is None:
             logging.fatal("{0} does not exist")
         job = Job(node_name)
         template = node.attrib.get('template')
-        property_dict = dict()
-        stack = []
-        for property in node.findall('property'):
-            text = property.text
-            name = property.attrib['name']
-            stack.append((name, text))
-        solved = True
-        while solved:
-            solved = False
-            name, text = stack.pop()
-            matches = re.findall("({{\s*\w+\s*}})",text)
-            if len(matches) == 0:
-                property_dict[name] = text
-                solved = True
-            else:
-                for match in matches:
-                    properties = re.findall("\w+", match)
-                    current_solved = 0
-                    if property in property_dict.keys():
-                        text = re.sub(match, property_dict[property], text)
-                        logging.debug("{} replaced by {}".format(match, text))
-                        solved = True
-                        current_solved += 1
-                    else:
-                        stack.append((name, text))
-                        break
-                    property_dict[name] = text
-        if len(stack) != 0:
-            logging.fatal("Unsolved properties : {}".format(stack))
-        logging.info("Properties loaded : {}".format(property_dict))
+
+        properties = dict()
+        for prop in node.findall('const'):
+            text = prop.text
+            name = prop.attrib['name']
+            properties[name] = {'text': text, 'solved': False}
+        ConfigurationManager.load_properties(properties)
+        self.load_instance(job, template, properties)
+
         return job
 
 
